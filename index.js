8765bb73ffc3fde399098dc52acb0c87732b5186
Francesca Gravatt <francesca.gravatt@gmail.com>
2022-05-05 17:29:25 -0500

    commit tree ac0a1fd9fae5b29982b47c1ddf5cd50886e67c09

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    const cards = []
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  console.log(cards);
+
+  return(cards);
+  
+}
+
+writeCards;
+
+function countDown(num) {
+    let loop = num
+    while (loop >= 0) {
+        console.log(loop--);
+    }
+}
+    
